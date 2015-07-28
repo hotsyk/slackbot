32ba5c7d0abddab8a8422d5b4e6b80d4a18c1eca
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-07-28 20:31:14 +0300

    Wait for result from build

--- slackbot/plugins/build.py
+++ slackbot/plugins/build.py
@@ -33,9 +33,12 @@ def stage_build(message):
         my_job = J['stage']
         res = my_job.invoke()
         message.send('> 🔥 Started build {0}'.format(res))
+        build_in_progress = False
+        return wait_till_end_of_build(message)
     else:
         message.send('>⛔ Stage build was halted')
-    build_in_progress = False
+        build_in_progress = False
+        return
 
 
 @listen_to('fuck')
@@ -55,6 +58,7 @@ def stop_stage_build(message):
                       'But you can cancel stage build with '
                       '"@buildbot: stop stage build".\n'
                       'Check help for more information')
+    return
 
 
 @respond_to('stop stage build')
@@ -71,3 +75,27 @@ def stop_running_build(message):
         message.reply('Build #{0} was stopped'.format(builds[0]))
     else:
         message.reply('Running build was not found')
+    return
+
+
+def wait_till_end_of_build(message):
+    J = Jenkins(settings.JENKINS_URL,
+                username=settings.JENKINS_USERNAME,
+                password=settings.JENKINS_PASSWORD)
+    my_job = J['stage']
+    builds = [i for i in my_job.get_build_ids()]
+    build = my_job.get_build(builds[0])
+    build_started = build.is_running()
+    while build.is_running():
+        sleep(10)
+
+    if build_started:
+        status = build.get_status()
+        if status == 'SUCCESS':
+            message.send('>✅ Build "{0}" finished.\n>'
+                         'Check results here {1}'.format(
+                             build.name, build.get_result_url()))
+        else:
+            message.send(
+                '>❗ Build "{0}" failed\n>Check results here {1}'.format(
+                    build.name, build.get_result_url()))
